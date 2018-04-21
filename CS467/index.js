56f91c46d2a4b2f9e8b582f73e96d04bce42ccb8
Adam B <brulotta@oregonstate.edu>
2018-04-21 18:22:05 -0400

    initial commit to project

---
+++ CS467/index.js
@@ -0,0 +1,13 @@
+﻿const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("hello world!");
+});
+
+const server = app.listen(8080, () => {
+	const host = server.address().address;
+	const port = server.address().port;
+
+	console.log(`Example app listening at http://${host}:${port}`);
+});
